808edd66eb31fecbf0b524217b22f90574fd74c4
frankjlin16 <flin30@asu.edu>
2021-03-13 11:37:17 -0700

    Updated Image model __str__().

--- vision/models.py
+++ vision/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 # Create your models here.
-class Images(models.Model):
+class Image(models.Model):
     image = models.ImageField(upload_to='uploads/images/')
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.image.name
