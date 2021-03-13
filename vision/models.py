9f09bb37481f297a62bc87e7148c84acd8c5863e
frankjlin16 <flin30@asu.edu>
2021-03-13 11:19:33 -0700

    Migrated Image model.

--- vision/models.py
+++ vision/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 # Create your models here.
 class Images(models.Model):
-    image = models.ImageField()
+    image = models.ImageField(upload_to='uploads/images/')
     date = models.DateTimeField(auto_now_add=True)
