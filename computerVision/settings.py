1dfd213ce986352ff113dfb2ce18ec7c7146df0a
frankjlin16 <flin30@asu.edu>
2021-03-13 18:22:22 -0700

    Set DEBUG based on environment

--- computerVision/settings.py
+++ computerVision/settings.py
@@ -36,7 +36,6 @@ STATICFILES_DIRS = [
 INSTALLED_APPS = [
     # My apps
     'vision',
-    'whitenoise.runserver_nostatic',
 
     # Default Django apps
     'django.contrib.admin',
@@ -49,7 +48,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
