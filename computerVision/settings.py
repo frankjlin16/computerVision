1096321ac6aa995f89fc5a2ef65636698819b080
frankjlin16 <flin30@asu.edu>
2021-03-13 22:43:26 -0700

    Updated settings

--- computerVision/settings.py
+++ computerVision/settings.py
@@ -126,7 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_ROOT = "/var/www/example.com/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
